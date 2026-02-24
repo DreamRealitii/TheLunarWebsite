02bd0780fa17ae7b2f870d9db246292277d17c1c
The Lunar Carver <ethancarnahan@gmail.com>
2026-02-23 21:06:59 -0800

    Time to make it mine

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeroesComponent } from './heroes/heroes.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+//import { HeroesComponent } from './heroes/heroes.component';
+//import { DashboardComponent } from './dashboard/dashboard.component';
+//import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'heroes', component: HeroesComponent },
-  { path: 'detail/:id', component: HeroDetailComponent }
+  { path: '', component: HomeComponent },
+  //{ path: 'dashboard', component: DashboardComponent },
+  //{ path: 'heroes', component: HeroesComponent },
+  //{ path: 'detail/:id', component: HeroDetailComponent }
 ];
 
 @NgModule({
