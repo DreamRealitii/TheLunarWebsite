367517e910fe32cb0c7582b5a91898fa472d9110
The Lunar Carver <ethancarnahan@gmail.com>
2026-02-24 17:22:35 -0800

    Smooth looking icon

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,25 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   @ViewChild('image') image : ElementRef | undefined;
-  rotation: number = 0;
+  currentRotation: number = 0;
+  correctRotation: number = 0;
+
+  ngOnInit(): void {
+    setInterval(() => {
+      let moveTowards: number = this.correctRotation;
+      if (moveTowards - this.currentRotation >= Math.PI)
+        moveTowards -= Math.PI * 2;
+      if (this.currentRotation - moveTowards >= Math.PI)
+        moveTowards += Math.PI * 2;
+      this.currentRotation = ((this.currentRotation * 0.95) + (moveTowards * 0.05)) % (Math.PI * 2)
+    }, 10)
+  }
 
   onMouseMove(pe: PointerEvent): void {
     const element: HTMLElement = this.image?.nativeElement;
@@ -16,6 +28,6 @@ export class HomeComponent {
     const centerY: number = rect.top + (rect.height / 2);
     const diffX: number = pe.clientX - centerX;
     const diffY: number = pe.clientY - centerY;
-    this.rotation = Math.atan2(diffY, diffX);
+    this.correctRotation = Math.atan2(diffY, diffX);
   }
 }
