07e8b8712454a58d0a0e06da85127db48078352b
The Lunar Carver <ethancarnahan@gmail.com>
2026-02-28 10:52:29 -0800

    Finish hobbies

--- src/app/hobbies/hobbies.component.ts
+++ src/app/hobbies/hobbies.component.ts
@@ -15,16 +15,25 @@ export class HobbiesComponent {
       embedSrc: 'https://www.youtube.com/embed/CdOt-YLdObU?si=uUYQYAESuYh_QDw1'
     },
     {
-      title: 'Watching YouTube',
-      description: 'I am thankful that Markiplier is the creator I primarily grew up on, as opposed to someone like Mr. Beast. Nowadays, I would say my favorite channel for gaming content is Teo, my favorite for improv comedy is SnapCube, and my favorite for scripted comedy is Something Witty Entertainment. I also watch a few progressive political pundits, but I won\'t say who.'
+      title: 'Play Other Games',
+      description: 'Growing up with Terraria was so fun it made most other adventure/RPG games unplayable for me. I can enjoy other genres every once in a while, like Celeste for the platforming, or RPG Maker horror games for the vibes.',
+      imageSrc: '/assets/Games.png'
     },
     {
-      title: 'Reading',
-      description: 'About 90% of what I read is comics of the "yuri" genre. It is such an improvement over the action/fantasy shows I grew up watching. My favorite authors for traditional series are Yuama and Xian Jun, and Morishima Akiko is very underrated. I have a Discord server where I share grades for everything I\'ve read, but it\'s private for now.'
+      title: 'Watch YouTube',
+      description: 'I am eternally thankful that Markiplier is the creator I grew up on, as opposed to someone like Mr. Beast. Nowadays, I would say my favorite channels are Teo for gaming content, SnapCube for improv comedy, and Something Witty Entertainment for scripted comedy. I also watch a few progressive political pundits, but I won\'t say who.',
+      imageSrc: '/assets/YT_Channels.png'
+    },
+    {
+      title: 'Read',
+      description: 'About 90% of what I read are comics in the "yuri" genre. It is such an improvement over the action/fantasy shows I grew up watching. My favorite authors for traditional series are Yuama and Xian Jun, and Morishima Akiko is very underrated. I have a Discord server where I share grades for everything I\'ve read, but it\'s private for now.',
+      link: "https://dynasty-scans.com/lists/454447-yuri-but-it-s-only-peak-sfw?view=tags",
+      imageSrc: '/assets/Reading.png'
     },
     {
       title: 'Music',
-      description: 'About 90% of what I listen to is video game soundtracks. Outside of that, my favorite genre is breakcore.'
+      description: 'About 90% of what I listen to is video game soundtracks. Outside of that, my favorite genre is breakcore. I use the Moondrop Blessing 3 along with an old Schiit stack (yes that is their name). I also EQ and crossfeed my headphones to get the most accurate possible sound.',
+      imageSrc: '/assets/Music.png'
     }
   ];
 }
