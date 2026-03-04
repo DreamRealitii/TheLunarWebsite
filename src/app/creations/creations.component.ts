7d71b464486634b46e3a5452dc1410d0345601e8
The Lunar Carver <ethancarnahan@gmail.com>
2026-03-04 00:39:05 -0800

    Images load now.

--- src/app/creations/creations.component.ts
+++ src/app/creations/creations.component.ts
@@ -12,37 +12,37 @@ export class CreationsComponent {
       title: 'A Smart Music Recommender',
       description: 'A work-in-progress freemium cloud app that compares and recommends songs based on objective sound data instead of artist, genre, or user activity. Currently developing using .NET Aspire.',
       link: 'https://docs.google.com/document/d/1y3B88Gyh9hmKGk-Ntyka1RM_WMpR4c9phLWGx6TTuTE/edit?usp=sharing',
-      imageSrc: '/assets/ASMR.png'
+      imageSrc: './assets/ASMR.png'
     },
     {
       title: 'Chess Visualized',
       description: 'My own Chess game that displays every move each piece can make, and what effects they have on the game. Developed from scratch using .NET MAUI.',
       link: 'https://store.steampowered.com/app/3513790/Chess_Visualized/',
-      imageSrc: '/assets/CV.png'
+      imageSrc: './assets/CV.png'
     },
     {
       title: 'This Website',
       description: 'A simple website I made in Angular to share myself, along with proving I can also do front-end development and CI/CD.',
       link: 'https://github.com/DreamRealitii/TheLunarCarver-Website',
-      imageSrc: '/assets/Website.png'
+      imageSrc: './assets/Website.png'
     },
     {
       title: 'Beat Saber Levels',
       description: 'Custom levels made for the VR game Beat Saber, which have been played by thousands of people. Some levels are also "curated" by BeatSaver staff and "ranked" by the ScoreSaber/BeatLeader teams.',
       link: 'https://beatsaver.com/profile/1600',
-      imageSrc: '/assets/BS_Levels.png'
+      imageSrc: './assets/BS_Levels.png'
     },
     {
       title: 'Beat Saber Mods',
       description: 'I made a couple Unity mods in C# for Beat Saber: One called Bad Cut Type for the game itself, and more recently one called BPM Helper for a level editor.',
       link: 'https://github.com/DreamRealitii/ChroMapper-BPMHelper',
-      imageSrc: '/assets/BPM.png'
+      imageSrc: './assets/BPM.png'
     },
     {
       title: 'YouTube Channel',
       description: 'As a teenager, I tried going the route of the gaming YouTuber. Although it never turned into a source of money or clout, what I made back then was still decently entertaining and helped me learn to edit.',
       link: 'https://www.youtube.com/@DreamRealitii',
-      imageSrc: '/assets/YT.png'
+      imageSrc: './assets/YT.png'
     }
   ];
 }
--- src/app/hobbies/hobbies.component.ts
+++ src/app/hobbies/hobbies.component.ts
@@ -17,23 +17,23 @@ export class HobbiesComponent {
     {
       title: 'Play Other Games',
       description: 'Growing up with Terraria was so fun it made most other adventure/RPG games unplayable for me. I can enjoy other genres every once in a while, like Celeste for the platforming, or RPG Maker horror games for the vibes.',
-      imageSrc: '/assets/Games.png'
+      imageSrc: './assets/Games.png'
     },
     {
       title: 'Watch YouTube',
       description: 'I am eternally thankful that Markiplier is the creator I grew up on, as opposed to someone like Mr. Beast. Nowadays, I would say my favorite channels are Teo for gaming content, SnapCube for improv comedy, and Something Witty Entertainment for scripted comedy. I also watch a few progressive political pundits, but I won\'t say who.',
-      imageSrc: '/assets/YT_Channels.png'
+      imageSrc: './assets/YT_Channels.png'
     },
     {
       title: 'Read',
       description: 'About 90% of what I read are comics in the "yuri" genre. It is such an improvement over the action/fantasy shows I grew up watching. My favorite authors for traditional series are Yuama and Xian Jun, and Morishima Akiko is very underrated. I have a Discord server where I share grades for everything I\'ve read, but it\'s private for now.',
       link: "https://dynasty-scans.com/lists/454447-yuri-but-it-s-only-peak-sfw?view=tags",
-      imageSrc: '/assets/Reading.png'
+      imageSrc: './assets/Reading.png'
     },
     {
       title: 'Music',
       description: 'About 90% of what I listen to is video game soundtracks. Outside of that, my favorite genre is breakcore. I use the Moondrop Blessing 3 along with an old Schiit stack (yes that is their name). I also EQ and crossfeed my headphones to get the most accurate sound possible.',
-      imageSrc: '/assets/Music.png'
+      imageSrc: './assets/Music.png'
     }
   ];
 }
