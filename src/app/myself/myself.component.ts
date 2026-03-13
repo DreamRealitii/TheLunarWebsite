b2a5bfed904f962def588ce79cb4a62adece4f22
The Lunar Carver <ethancarnahan@gmail.com>
2026-03-13 16:02:10 -0700

    Keep tabs on myself

--- src/app/myself/myself.component.ts
+++ src/app/myself/myself.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
     selector: 'app-myself',
     templateUrl: './myself.component.html',
     styleUrls: ['./myself.component.css'],
+    imports: [MatTabsModule]
 })
 export class MyselfComponent {
 
